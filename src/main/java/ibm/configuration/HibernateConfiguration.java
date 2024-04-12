591eed511559fddd5a3d4e669a8d5ede9fd448b8
pancham01 <pancham9557@gmail.com>
2024-04-12 17:03:17 +0530

    update

---
+++ src/main/java/ibm/configuration/HibernateConfiguration.java
@@ -0,0 +1,28 @@
+package ibm.configuration;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+public class HibernateConfiguration {
+	
+	public static SessionFactory getSessionFactory()
+	{
+		Properties prop = new Properties();
+		prop.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		prop.put(Environment.URL, "jdbc:mysql://localhost:3306/ducat");
+		prop.put("hibernate.connection.username", "root");
+		prop.put(Environment.PASS, "root");
+		prop.put(Environment.SHOW_SQL, "true");
+		prop.put(Environment.FORMAT_SQL, "true");
+		prop.put(Environment.HBM2DDL_AUTO, "create");
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(prop).build();
+		MetadataSources meta = new MetadataSources(ssr);
+		meta.addAnnotatedClassName("ibm.entity.Student");
+		SessionFactory sf = meta.buildMetadata().buildSessionFactory();
+		return sf;
+	}
+}
--- src/main/java/ibm/entity/Student.java
+++ src/main/java/ibm/entity/Student.java
@@ -1,12 +1,21 @@
 package ibm.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "DucatStudent")
+//@Table(name="ducatStud")
 public class Student {
+	@Id
 	private int id;
+	@Column(name = "d_name")
 	private String name;
+	@Column(name = "d_gender")
 	private String Gender;
+	@Column(name = "d_add")
 	private String address;
-	
-	
+
 	public Student(int id, String name, String gender, String address) {
 		super();
 		this.id = id;
@@ -14,33 +23,42 @@ public class Student {
 		Gender = gender;
 		this.address = address;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getGender() {
 		return Gender;
 	}
+
 	public void setGender(String gender) {
 		Gender = gender;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", Gender=" + Gender + ", address=" + address + "]";
 	}
-	
+
 }
